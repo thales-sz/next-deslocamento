e1f66bda5fd875161f8b492421df0b63e9caf325
Thales <thales.souz@outlook.com>
2023-06-20 10:24:36 -0300

    feat: header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,29 @@
 "use client"
 
 import Header from "@/components/header"
-import { Button } from "@mui/material"
-import Link from "next/link"
+import { Button, ThemeProvider, createTheme } from "@mui/material"
+
+const theme = createTheme({
+  palette: {
+    primary: { 
+      main: "#00ADB5",
+    },
+    secondary: {
+      main: '#11cb5f',
+    },
+  },
+});
 
 export default function Home() {
   return (
-      <main>
-        <Header />
-        <Button variant="contained">
-          <Link href={'/home'}>Hello World</Link>
-        </Button>
-        Esta é a página inicial!
+      <main className="flex min-h-screen flex-col items-center justify-between bg-tertiary">
+        <ThemeProvider theme={theme}>
+          <Header />
+          <Button variant="contained" href="/home">
+            Hello World
+          </Button>
+          Esta é a página inicial!
+        </ThemeProvider>
       </main>
   )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,24 @@
+import { AppBar, Box, Button, ButtonGroup, IconButton, Toolbar, Typography } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu';
 import React, { FC } from 'react'
 
 interface headerProps {}
 
 const Header: FC<headerProps> = () => {
   return (
-    <header className='w-full bg-slate-100 text-black'>
-      Header
+    <header className='flex w-full text-quaternary p-10'>
+      <Box sx={{ flexGrow: 1 }} alignItems="center">
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              FrontEnd Challenge
+            </Typography>
+            <Button color="inherit">Login</Button>
+            <Button color="inherit">Login</Button>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </header>
   )
 }
